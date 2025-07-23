6378e6a30e94d6ef1ed918380dc826a676ac8345
Mairaj Shaikh <shaikhmairaj075@gmail.com>
2025-07-23 19:06:37 +0500

    Add files via upload

---
+++ sql_query_checks.sql
@@ -0,0 +1,15 @@
+SELECT @@SERVERNAME;
+
+Use BikeStore;
+
+
+SELECT * FROM sales.customers;
+
+SELECT * FROM hr.candidates; 
+
+SELECT * FROM production.brands;
+
+SELECT * FROM production.parts;
+
+
+
